02db11f86aff524d74f2ce2d5eb016971aa370cf
clydexmarty09 <cmarty@ucsd.edu>
2026-03-25 14:37:03 -0700

    added register button and changed up the overall theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-dvh bg-gray-300/60"> 
+    <div className="min-h-dvh"> 
       <main className="flex justify-center items-center relative w-full flex-col min-h-dvh overflow-hidden max-w-sm mx-auto"> 
 
         <h1 className="text-center font-bold text-2xl"> WORKOUT TRACKER </h1>
-        <p className="text-center text-xs text-gray-600 py-4"> Track your workouts, check your progress, and set goals. </p>
+        <p className="text-center text-xs text-gray-300 py-4"> Track your workouts, check your progress, and set goals. </p>
         
-        <div className="w-full items-center absolute bottom-4 left-0 flex flex-col gap-4 px-3">
-          <Link className="text-center text-sm w-full p-1 border rounded-md border-gray-700" href="register"> REGISTER </Link>
-          <Link className="text-center text-sm w-full p-1 border rounded-md border-gray-700" href="sign-in"> SIGN IN</Link>
+        <div className="w-full items-center absolute bottom-4 left-0 flex flex-col gap-4 py-3 px-3">
+          <Link className="btn" href="register"> REGISTER </Link>
+          <Link className="btn" href="sign-in"> SIGN IN</Link>
         </div> 
       </main>
     </div>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,6 @@
 "use client"; 
 import { useState } from "react"; 
+import Link from "next/link"; 
 
 export default function Register() {
 
@@ -9,35 +10,41 @@ export default function Register() {
     
 
     return (
-        <div className="min-h-dvh bg-gray-300/60"> 
+        <div className="overflow-hidden min-h-dvh"> 
             <main className="w-full p-4 max-w-sm"> 
                 <h1 className="text-left font-bold text-2xl"> REGISTER </h1>
                 
-                <div className="gap-2 flex flex-col"> 
-                    <input
-                    className="p-1 border border-gray-500 rounded-md w-full"
-                    type="text"
-                    placeholder="Username"
-                    value={user}
-                    onChange={(e)=> setUser(e.target.value)}
-                    />
+                <form> 
+                    <div className="gap-2 flex flex-col"> 
+                        <input
+                        className="p-1 border border-gray-500 rounded-md w-full"
+                        type="text"
+                        placeholder="Username"
+                        value={user}
+                        onChange={(e)=> setUser(e.target.value)}
+                        />
 
-                    <input
-                     className="p-1 border border-gray-500 rounded-md w-full"
-                     type="text"
-                     placeholder="Password"
-                     value={pw}
-                     onChange={(e)=> setPw(e.target.value)}
-                     />
+                        <input
+                        className="p-1 border border-gray-500 rounded-md w-full"
+                        type="text"
+                        placeholder="Password"
+                        value={pw}
+                        onChange={(e)=> setPw(e.target.value)}
+                        />
 
-                     <input
-                      className="p-1 border border-gray-500 rounded-md w-full"
-                      type="email"
-                      placeholder="Email Address"
-                      value={email}
-                      onChange={(e)=> setEmail(e.target.value)}
-                      />  
-                </div> 
+                        <input
+                        className="p-1 border border-gray-500 rounded-md w-full"
+                        type="email"
+                        placeholder="Email Address"
+                        value={email}
+                        onChange={(e)=> setEmail(e.target.value)}
+                        />  
+
+                        <button className="btn" type="submit"> REGISTER </button>
+                    </div>
+                </form> 
+
+                <p> <Link className="text-blue-500" href="login"> Login </Link> instead </p> 
                 
             </main>
         </div>

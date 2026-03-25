a05fb47dabd594f9ce7631a92b51aec347f11131
clydexmarty09 <cmarty@ucsd.edu>
2026-03-25 16:07:32 -0700

    fixed regisration route and wired frontend with it

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,20 +1,59 @@
 "use client"; 
 import { useState } from "react"; 
 import Link from "next/link"; 
+import { useRouter } from "next/navigation"; 
 
 export default function Register() {
 
     const [email, setEmail] = useState("");
     const [pw, setPw] = useState("");
     const [user, setUser] = useState(""); 
+    const [error, setError] = useState(""); 
+    const [loading, setLoading] = useState(false); 
+
+    const router = useRouter(); 
+
     
+    async function handleSubmit(e: any) {
+       
+        e.preventDefault(); 
+        setError(""); 
+        setLoading(true); 
+
+        try {
+            const res = await fetch("/api/auth/register", {
+                method: "POST", 
+                headers: {
+                    "Content-Type": "application/json", 
+                }, 
+                body:JSON.stringify({
+                    email, pw, user, 
+                })
+            }); 
+
+            const data = await res.json(); 
+           
+            if(!res.ok) {
+                setError(data.error || "Registration failed"); 
+                return; 
+            }
+
+            console.log(data); 
+            router.push("/login")
+
+        } catch {
+            setError("Failed to register"); 
+        } finally {
+            setLoading(false); 
+        }
+    }
 
     return (
         <div className="overflow-hidden min-h-dvh"> 
             <main className="w-full p-4 max-w-sm mx-auto"> 
                 <h1 className="text-left font-bold text-2xl"> REGISTER </h1>
                 
-                <form> 
+                <form onSubmit={handleSubmit}> 
                     <div className="gap-2 flex flex-col"> 
                         <input
                         className="input"
@@ -26,7 +65,7 @@ export default function Register() {
 
                         <input
                         className="input"
-                        type="text"
+                        type="password"
                         placeholder="Password"
                         value={pw}
                         onChange={(e)=> setPw(e.target.value)}

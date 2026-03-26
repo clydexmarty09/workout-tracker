8c7713fb233c631bf975cc92a3cc9d83d73c6187
clydexmarty09 <cmarty@ucsd.edu>
2026-03-25 19:10:36 -0700

    updated login route to use cookies

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -17,7 +17,7 @@ export async function POST(request: Request) {
     const res = await db.query(
       `SELECT id, username, email, password_hash
             FROM users
-            WHERE username = ? or email = ?
+            WHERE username = $1 OR email = $2
             LIMIT 1`,
       [login],
     );
@@ -39,7 +39,27 @@ export async function POST(request: Request) {
       );
     }
 
-    return NextResponse.json({ ok: true, userId: user.id });
+    const sessionId = crypto.randomUUID();
+    const expires_at = new Date();
+    expires_at.setDate(expires_at.getDate() + 7);
+
+    await db.query(
+      `INSERT INTO sessions (id, user_id, expires_at)
+      VALUES ($1, $2, $3)`,
+      [sessionId, user.id, expires_at],
+    );
+
+    const response = NextResponse.json({ ok: true, userId: user.id });
+
+    response.cookies.set("sessionId", sessionId, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      expires: expires_at,
+      path: "/",
+    });
+
+    return response;
   } catch (error) {
     console.error("LOGIN ERROR", error);
     return NextResponse.json({ error: "Login failed" }, { status: 500 });

047b333cb697f0da43a815eb5a8b7eff64c1d40e
clydexmarty09 <cmarty@ucsd.edu>
2026-03-26 16:28:50 -0700

    Added exercises route

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -1,8 +1,20 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET(reques: Request) {
+export async function GET(request: Request) {
   try {
+    const { searchParams } = new URL(request.url);
+    const search = searchParams.get("search") || "";
+
+    const res = await db.query(
+      `SELECT id, name
+        FROM exercises
+        WHERE name ILIKE $1
+        ORDER BY name ASC`,
+      [`%${search}%`],
+    );
+
+    return NextResponse.json(res.rows);
   } catch (error) {
     console.error("Exercise searc error", error);
     return NextResponse.json(

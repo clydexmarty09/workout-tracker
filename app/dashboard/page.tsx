4f8404b89c2e69fa6b615a9e623498cbcebfb619
clydexmarty09 <cmarty@ucsd.edu>
2026-03-27 09:59:58 -0700

    added function for exercisessearch

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,46 @@
 "use client"; 
 import { useState } from "react"; 
 
+// we need this because the results are objects 
+type Exercise = {
+    id: number; 
+    name: string; 
+}
+
 export default function DashBoard() {
 
     const [showAddWorkout, setShowAddWorkout] = useState(false); 
     const [workoutName, setWorkoutName] = useState(""); 
     const [workoutLabel, setWorkoutLabel] = useState("");
+    const [exerciseSearch, setExerciseSearch] = useState("") // stores whatever the user types in the search box 
+    const [exerciseResults, setExerciseResults] = useState<Exercise[]>([]);  // stores the matches the come back from API route
+    const [selectedExercises, setSelectedExercises] = useState<Exercise[]>([])  // stores the exercises the user picked for workout
+
+    // Frontend -> API -> DB -> API -> frontend 
+    async function handleExerciseSearch() {
+        try {
+            // make a request to URL and encode it to prevent broken URLS 
+            const res = await fetch(`/api/exercises?search=${encodeURIComponent(exerciseSearch)}`); 
+
+            const data = await res.json(); 
+            if(!res.ok) {
+                console.error(data.error || "Search failed"); 
+                return; 
+            }
+
+            setExerciseResults(data)
+        
+        } catch (error) {
+            console.error("Search failed.", error); 
+        }
+    }
 
     return(
         <div> 
             <main> 
                 <div> 
                     {!showAddWorkout ? (
-                        <button> Add Workout </button>
+                        <button onClick={()=>setShowAddWorkout(true)}> Add Workout </button>
                     ) : (
                         <div> 
                             <h2> Create workout </h2>

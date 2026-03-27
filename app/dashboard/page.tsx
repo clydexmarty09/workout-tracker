29cd1e1270ef0908ff3cc1dfc70111f33bad749f
clydexmarty09 <cmarty@ucsd.edu>
2026-03-27 09:40:25 -0700

    added frontend ui option to add a workout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,10 +2,42 @@
 import { useState } from "react"; 
 
 export default function DashBoard() {
+
+    const [showAddWorkout, setShowAddWorkout] = useState(false); 
+    const [workoutName, setWorkoutName] = useState(""); 
+    const [workoutLabel, setWorkoutLabel] = useState("");
+
     return(
         <div> 
             <main> 
-                <h1> IT WORKS </h1>
+                <div> 
+                    {!showAddWorkout ? (
+                        <button> Add Workout </button>
+                    ) : (
+                        <div> 
+                            <h2> Create workout </h2>
+                            
+                            <input
+                            value={workoutName}
+                            type="text"
+                            placeholder="Workout Name"
+                            onChange={(e)=> setWorkoutName(e.target.value)}
+                            />
+
+                            <input
+                             value={workoutLabel}
+                             type="text"
+                             placeholder="Label (optional)"
+                             onChange={(e)=> setWorkoutLabel(e.target.value)}
+                             />
+
+                             <div> 
+                                <button> Cancel </button>
+                                <button> Continue </button>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </main>
         </div>
     )

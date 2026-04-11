d4a1512e9aefcf6810ad2fb5ffaf3b2807eeb7e0
clydexmarty09 <cmarty@ucsd.edu>
2026-04-10 23:42:56 -0700

    added GET and POST routing on frontend. Also added option to create a workout

--- app/front-page/page.tsx
+++ app/front-page/page.tsx
@@ -3,14 +3,68 @@ import { useState } from "react";
 
 export default function FrontPage() {
 
-    const [exercisesName, setExerciseName] = useState(""); 
+    const [exerciseName, setExerciseName] = useState(""); 
     const [error, setError] = useState(""); 
     const [loading, setLoading] = useState(false); 
-    const [exercises, setExercises] = useState([]); 
+    const [exercises, setExercises] = useState<any[]> ([]); 
+
+    // for creating workouts 
+    const [selectedExercises, setSelectedExercises] = useState<any[]>([]); 
 
     async function handleAddExercise() {
 
+        try {
+            const res = await fetch(`/api/exercises`,  // send request to backend route 
+                {
+                    method: "POST", 
+                    headers: {
+                        "Content-Type": "application/json"  // format of the request body 
+                    }, 
+                    body : JSON.stringify({ name: exerciseName })  // take JS object and convert to a JSON string 
+                }, 
+            );
 
+            const data = await res.json(); // reads JSON data the backend sent back 
+            
+            if(!res.ok) {
+                setError(data.error || "Cannot add exercise"); 
+                return; 
+            }
+
+            setExerciseName(""); 
+            setError(""); 
+            await fetchExercises(); 
+
+        } catch (error) {
+            setError("Cannot add exercise"); 
+        }
+    }
+
+    async function fetchExercises() {
+        try {
+
+            setLoading(true); 
+            const res = await fetch(`/api/exercises`); 
+            const data = await res.json(); 
+            
+            if (!res.ok) {
+                setError("Cannot fetch exercises ")
+                return; 
+            }
+
+            setExercises(data); 
+            console.log(data); 
+            setError(""); 
+       
+        } catch {
+            setError("Cannot fetch exercises"); 
+        } finally {
+            setLoading(false); 
+        }
+    }
+
+    function addToWorkout(exercise: any) {
+        setSelectedExercises((prev)=> [...prev, exercise])
     }
 
     return (
@@ -19,7 +73,7 @@ export default function FrontPage() {
            
             <form> 
                 <input 
-                value={exercisesName}
+                value={exerciseName}
                 onChange={(e) => setExerciseName(e.target.value)}
                 placeholder="Insert exercises name"
                 type="text"
@@ -27,6 +81,29 @@ export default function FrontPage() {
 
                 <button type="button" onClick={handleAddExercise}> Add Exercises </button> 
             </form>
+
+            <div> 
+                <button type="button" onClick={fetchExercises}> Show exercises</button>
+               
+                { loading && <p> Loading ... </p>}
+                { error && <p> { error} </p>}
+                { exercises.map((e)=> (
+                    <div key={e.id}> 
+                        <p> {e.name} </p>
+                        <button type="button" onClick={()=>  addToWorkout(e)}> Add to workout </button>
+                    </div> 
+                ))}
+                
+            </div>
+
+            <div> 
+                <h2> Current Workout </h2>
+                { selectedExercises.map((exercise)=> (
+                    <div key={exercise.id}> 
+                        <p> { exercise.name} </p>
+                    </div> 
+                ))}
+            </div>
         </main>
     )
 }

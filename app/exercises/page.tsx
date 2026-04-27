d677c88aac7006f86d1906ba62390b3db0efdd06
clydexmarty09 <cmarty@ucsd.edu>
2026-04-26 19:18:06 -0700

    added basic styling to exercise and workout pages

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -2,9 +2,11 @@
 
 export default function Exercises() {
     return (
-        <main> 
+        <main className="min-h-dvh bg-black text-white">
+            <div className="flex flex-col gap-4 p-4 mx-auto max-w-md">
 
-            <h1> It works! </h1>
+                <h1> It works! </h1>
+            </div> 
         </main>
     )
-}
+}}

7c0ce5f215b2d7b0f478dcd8ebcce350711eadf7
clydexmarty09 <cmarty@ucsd.edu>
2026-05-01 20:40:32 -0700

    moved logout button to navbar and cleaning up the code

--- app/components/BottomNav.tsx
+++ app/components/BottomNav.tsx
@@ -1,10 +1,41 @@
 "use client"; 
 
 import Link from "next/link"; 
-import { usePathname } from "next/navigation";  // lets us know what page the user is on 
+import { useState } from "react"; 
+import { usePathname, useRouter } from "next/navigation";  // lets us know what page the user is on 
 
 export default function BottomNav() {
     const pathname = usePathname();   // store the current path url 
+    const [loading, setLoading] = useState(false); 
+    const [error, setError] = useState<string>(""); 
+    const router = useRouter(); 
+    
+
+    async function handleLogout() {
+        try {
+
+            setError(""); 
+            setLoading(true); 
+
+            const res = await fetch(`/api/auth/logout`, 
+                {
+                    method: "POST"
+                }
+            ); 
+
+            if (!res.ok) {
+                setError("Log out failed")
+                return; 
+            }
+
+            router.push('/login'); 
+
+        } catch {
+            setError("Unauthorized"); 
+        } finally {
+            setLoading(false); 
+        }
+    }
 
     return (
         <nav className="fixed z-50 bottom-0 left-0 w-full border-t bg-black"> 
@@ -35,6 +66,16 @@ export default function BottomNav() {
                 > 
                 WORKOUTS
                 </Link>
+
+                <button
+                    type="button"
+                    onClick={handleLogout}
+                    className={`flex flex-col items-center text-xs font-medium transition ${
+                        pathname === "/login" ? "text-green-500" : "text-gray-400"
+                    }`}
+                >
+                    LOGOUT
+                </button>
             </div>
         </nav>
     ); 
--- app/main/front-page/page.tsx
+++ app/main/front-page/page.tsx
@@ -12,12 +12,6 @@ export default function FrontPage() {
     const [sessionCount, setSessionCount] = useState(0); 
     const [activeSession, setActiveSession] = useState<any | null>(null); 
 
-    // console.log(activeSession.id); 
-       
-    // for tabbing the other sections of the site 
-    
-    // const [showExercises, setShowExercise] = useState(false);
-    // const [showWorkouts, setShowWorkouts] = useState(false); 
 
     useEffect(()=> {
         async function fetchData() {
@@ -61,55 +55,6 @@ export default function FrontPage() {
     // }
 
     const router = useRouter(); // for navigation
-    async function handleLogout() {
-        try {
-
-            setError(""); 
-            setLoading(true); 
-
-            const res = await fetch(`/api/auth/logout`, 
-                {
-                    method: "POST"
-                }
-            ); 
-
-            if (!res.ok) {
-                setError("Log out failed")
-                return; 
-            }
-
-            router.push('/login'); 
-
-        } catch {
-            setError("Unauthorized"); 
-        } finally {
-            setLoading(false); 
-        }
-    }
-   
-
-    // this function gets all exercises from the backend and stores them in state 
-    // this is for creating a temporary workout 
-    // function addToWorkout(exercise: any) {
-    //     // we use prev because it's the value of the state before any updates 
-    //      setSelectedExercises((prev)=> {
-    //         const exists = prev.some((item)=> item.id === exercise.id); 
- 
-    //         if(exists) {
-    //             return prev; 
-    //         }
-
-    //         return [...prev, exercise]; 
-    //      }); 
-    
-    // }
-
-    // function deleteExercises(id: number) {
-    //     // filter because we create a new array only passing the given condition 
-    //     setSelectedExercises((prev)=> 
-    //         prev.filter((item)=> item.id !== id)
-    //     ); 
-    // }
 
     return (
 
@@ -124,13 +69,6 @@ export default function FrontPage() {
                     <h1 className="text-xl font-bold">Workout Tracker</h1>
                     </div>
 
-                    <button
-                    type="button"
-                    onClick={handleLogout}
-                    className="rounded-full border border-zinc-700 px-3 py-1.5 text-sm text-zinc-300"
-                    >
-                    Exit
-                    </button>
                 </div>
             </header>
 
@@ -159,103 +97,6 @@ export default function FrontPage() {
                     </div> 
                 </div> 
             </section>
-    
-        
-            {/* <section className="p-3">
-                <div className="card"> 
-                    <button className="my-2" type="button" onClick={()=> {
-                        setShowExercise(prev=>!prev); 
-                        fetchExercises(); 
-                    }}>
-
-                    <h3 className="underline text-lg font-semibold text-left"> {showExercises? "Hide": "Show Exercises"} </h3>
-                     </button> 
-
-                  
-                
-                    { showExercises && (
-                    <>
-                        { loading && <p> Loading ... </p>}
-                        { error && <p className="text-red-500"> { error} </p>}
-                        <h2 className="text-md"> Exercise List </h2>
-                        { exercises.map((e)=> (
-                            <div className="flex gap-3" key={e.id}> 
-                                <p className="flex-1"> {e.name} </p>
-                                <button className="btn-3" type="button" onClick={()=>  addToWorkout(e)}> [Add] </button>
-                            </div> 
-                        ))}
-                    
-                    
-                        <button type="button" className="btn-2 w-full" onClick={closeExercises}> Close </button>
-                  </>
-                  )}
-                    
-                </div>
-
-                </section> */}
-{/* 
-                <section className="p-3">
-
-                   <div className="card">
-                    <button type="button" onClick={()=> {
-                        setShowWorkouts(prev=>!prev); 
-                        fetchWorkouts(); 
-                    }}> 
-                    
-                        <h3 className="text-left underline text-lg font-semibold"> {showWorkouts ? "Hide": "Show Workouts"} </h3>
-                    </button>
-
-                    { showWorkouts && 
-                        <div className="flex flex-col gap-4"> 
-                            <h2 className="font-semibold text-2xl"> Current Workout </h2>
-                            { selectedExercises.map((exercise)=> (
-                                <div className="flex gap-3" key={exercise.id}> 
-                                    <p className="flex-1"> { exercise.name} </p>
-                                    <button className="btn-3 "type="button" onClick={()=> deleteExercises(exercise.id)}> [Delete] </button>
-                                </div> 
-                            ))}
-                            
-
-                            <input
-                            value={workoutName}
-                            onChange={(e)=> setWorkoutName(e.target.value)}
-                            placeholder="Workout Name"
-                            type="text"
-                            className="border border-amber-50 rounded-md p-2 w-full"
-                            /> 
-
-                            <input
-                            value={label}
-                            onChange={(e)=> setLabel(e.target.value)}
-                            placeholder="Workout Label (optional)"
-                            type="text"
-                            className="border border-amber-50 rounded-md p-2 w-full"
-                            />
-
-                            <button className="btn" type="button" onClick={handleCreateWorkout}> Create Workout </button>
-                        </div>
-
-                    }
-
-                    </div>
-            </section> */}
-
-
-            {/* <div className="card">  
-                <h2 className="text-left text-2xl font-semibold"> Workouts </h2>
-                { workouts.map((w)=> (
-                    <div className="flex text-left flex-col gap-3" key={w.id}> 
-                        <h3> {w.name} </h3>
-                        <p> {w.label} </p>
-                        <button className="btn-3" onClick={()=> handleAddSession(w.id)}> Start Workout </button>
-                        <button onClick={()=> handleDeleteWorkout(w.id)}type="button" className="btn-2"> Delete </button> 
-
-                        {w.exercises.map((exercise: any)=> (
-                            <div key={exercise.id}> {exercise.name} </div> 
-                        ))}
-                    </div>
-                ))}
-            </div> */}
 
             <div className="items-left flex flex-col p-4 my-2 gap-3 border rounded-md border-white/10">
 

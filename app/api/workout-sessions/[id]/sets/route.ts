34184fa4237f2a8dd0e9d8a70d59c39c164e3640
clydexmarty09 <cmarty@ucsd.edu>
2026-05-04 23:59:10 -0700

    added POST route in sets route file

--- app/api/workout-sessions/[id]/sets/route.ts
+++ app/api/workout-sessions/[id]/sets/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { getLoggedInUserId } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { requestToBodyStream } from "next/dist/server/body-streams";
 
 export async function POST(
   request: Request,
@@ -22,6 +23,25 @@ export async function POST(
         { status: 400 },
       );
     }
+
+    const sessionCheck = await db.query(
+      `SELECT id
+        FROM workout_sessions
+        WHERE id = $1 AND user_id = $2`,
+      [sessionId, userId],
+    );
+
+    if (sessionCheck.rows.length === 0) {
+      return NextResponse.json({ error: "Session not found" }, { status: 404 });
+    }
+
+    const res = await db.query(
+      `INSERT INTO session_sets (session_id, exercise_id, set_number, weight_lbs, reps)
+        VALUES ($1, $2, $3, $4, $5)`,
+      [sessionId, exerciseId, setNumber, weightLbs, repNumber],
+    );
+
+    return NextResponse.json(res.rows[0], { status: 201 });
   } catch {
     return NextResponse.json({ error: "Cannot update sets" }, { status: 500 });
   }

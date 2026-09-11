05c50a4c40019fe982839465c395aa558d2761b9
clydexmarty09 <cmarty@ucsd.edu>
2026-09-11 21:07:45 +0800

    cleaning up

--- app/api/workout-sessions/route.ts
+++ app/api/workout-sessions/route.ts
@@ -45,32 +45,6 @@ export async function POST(request: Request) {
     );
 
     return NextResponse.json(res.rows[0], { status: 201 });
-
-    // session.id
-    // const session = sessionRes.rows[0];
-
-    // // find all exercises that belong to the original workout template
-    // const exerciseRes = await db.query(
-    //   `SELECT exercise_id
-    //     FROM workout_exercises
-    //     WHERE workout_id = $1`,
-    //   [workoutId],
-    // );
-
-    // // start a loop over every exercises row returned by the previosu query
-    // // create one matching session_exercise row for each workout exercise
-    // // copy the exercises
-    // for (const exercise of exerciseRes.rows) {
-    //   await db.query(
-    //     `INSERT INTO session_exercises (session_id, exercises_id )
-    //         VALUES ($1, $2)`,
-    //     [session.id, exercise.exercise_id],
-    //   );
-    // }
-
-    // return NextResponse.json(session, { status: 201 });
-
-    //return NextResponse.json(res.rows[0], { status: 201 });
   } catch {
     return NextResponse.json(
       { error: "Cannot start workout session" },
